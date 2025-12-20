8fe42e264ddcfab996d34f3e685abf3d2e11ad4e
Magnetkopf <82427906+Magnetkopf@users.noreply.github.com>
2025-12-20 10:40:26 +0800

    feat: /info

--- main.go
+++ main.go
@@ -8,6 +8,8 @@ import (
 	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
 	"gopkg.in/yaml.v3"
 )
 
@@ -38,8 +40,10 @@ func main() {
 	// 注册回调函数
 	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
 		if i.Type == discordgo.InteractionApplicationCommand {
+			cmd := i.ApplicationCommandData().Name
 			// 时间戳 /timestamp
-			if i.ApplicationCommandData().Name == "timestamp" {
+			switch cmd {
+			case "timestamp":
 				timestamp := time.Now().Unix()
 				content := fmt.Sprintf("当前时间戳为: `%d`", timestamp)
 
@@ -52,7 +56,66 @@ func main() {
 				if err != nil {
 					log.Printf("响应失败: %v", err)
 				}
+			case "info":
+				// 硬件信息 /info
+				percents, _ := cpu.Percent(500*time.Millisecond, false)
+				cpuPercent := 0.0
+				if len(percents) > 0 {
+					cpuPercent = percents[0]
+				}
+
+				// CPU
+				cores, _ := cpu.Counts(true)
+				info, _ := cpu.Info()
+				model := "未知"
+				if len(info) > 0 {
+					model = info[0].ModelName
+				}
+				cpuContent := fmt.Sprintf("`%s`\n核心: `%d` 核\n负载: `%.2f%%`", model, cores, cpuPercent)
+
+				// RAM
+				v, err := mem.VirtualMemory()
+				var memContent string
+				if err == nil {
+					totalGB := float64(v.Total) / 1024 / 1024 / 1024
+					usedGB := float64(v.Used) / 1024 / 1024 / 1024
+					memContent = fmt.Sprintf("使用率: `%.2f%%` \n内存: `%.1fGB / %.1fGB`", v.UsedPercent, usedGB, totalGB)
+				} else {
+					memContent = "Error"
+				}
+
+				// Embed
+				embed := &discordgo.MessageEmbed{
+					Title:       "硬件监控",
+					Color:       0x00eaac,
+					Description: fmt.Sprintf("Bot所在服务器的硬件信息"),
+					Fields: []*discordgo.MessageEmbedField{
+						{
+							Name:   "💻 CPU",
+							Value:  cpuContent,
+							Inline: false,
+						},
+						{
+							Name:   "📟 RAM",
+							Value:  memContent,
+							Inline: false,
+						},
+					},
+					Footer: &discordgo.MessageEmbedFooter{
+						Text: "🕘",
+					},
+					Timestamp: time.Now().Format(time.RFC3339),
+				}
+
+				// Send embed
+				err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+					Type: discordgo.InteractionResponseChannelMessageWithSource,
+					Data: &discordgo.InteractionResponseData{
+						Embeds: []*discordgo.MessageEmbed{embed},
+					},
+				})
 			}
+
 		}
 	})
 
@@ -64,13 +127,23 @@ func main() {
 	defer dg.Close()
 
 	// 注册命令
-	command := &discordgo.ApplicationCommand{
-		Name:        "timestamp",
-		Description: "获取当前时间戳",
+	commands := []*discordgo.ApplicationCommand{
+		{
+			Name:        "timestamp",
+			Description: "获取当前时间戳",
+		},
+		{
+			Name:        "info",
+			Description: "获取硬件信息",
+		},
+		// add more commands here
 	}
-	_, err = dg.ApplicationCommandCreate(dg.State.User.ID, conf.GuildID, command)
-	if err != nil {
-		log.Fatalf("无法创建命令: %v", err)
+	for _, cmd := range commands {
+		_, err := dg.ApplicationCommandCreate(dg.State.User.ID, conf.GuildID, cmd)
+		if err != nil {
+			fmt.Printf("无法创建命令 %s: %w", cmd.Name, err)
+		}
+
 	}
 
 	fmt.Println("▶️ Running")
